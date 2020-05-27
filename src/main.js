e1ec8208fd86e583bf2c35671a1c4089beb688c7
lars_eliassen <lars@jobbnorge.no>
2020-05-27 15:13:25 +0200

    Quick 'n' dirty global.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/** Font Awesome Stuff **/
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+    faBullseye,
+    faThumbsUp,
+    faThumbsDown
+} from '@fortawesome/pro-solid-svg-icons';
+import {
+    faSortNumericDownAlt,
+    faSortNumericUp,
+    faSortAlphaUp,
+    faSortAlphaDownAlt,
+    faLongArrowUp,
+    faLongArrowDown,
+    faEraser
+} from '@fortawesome/pro-light-svg-icons'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(
+    faBullseye,
+    faThumbsUp,
+    faSortNumericUp,
+    faSortNumericDownAlt,
+    faThumbsDown,
+    faEraser,
+    faSortAlphaUp,
+    faSortAlphaDownAlt,
+    faLongArrowUp,
+    faLongArrowDown)
+Vue.component('fa-icon', FontAwesomeIcon)
+/** END Font Awesome Stuff **/
+
 Vue.config.productionTip = false
 
 new Vue({
