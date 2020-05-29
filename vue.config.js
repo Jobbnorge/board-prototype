51acc2825199dd1d42f4b6750da24e8a0bba8844
larseliassen <lars1986@gmail.com>
2020-05-29 13:14:40 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-    publicPath: '/board-prototype/'
+    outputDir: '\dist',
+    filenameHashing: false,
+    runtimeCompiler: true,
+    configureWebpack: {
+        optimization: {
+            splitChunks: false
+        },
+        resolve: {
+            alias: {
+                'vue$': 'vue/dist/vue.esm.js'
+            }
+        },
+        output: {
+            filename: 'board_vue_app.js'
+        }
+    },
+    chainWebpack: config => {
+        config.plugins.delete('html')
+        config.plugins.delete('preload')
+        config.plugins.delete('prefetch')
+    }
 }
